07d02734499d2a8750eeb9090ff27385683b6f91
Crem <maxcaminer1@gmail.com>
2024-05-28 15:32:29 +1000

    fixes

--- nix/rofi/powermenu/type-3/powermenu.sh
+++ nix/rofi/powermenu/type-3/powermenu.sh
@@ -10,7 +10,7 @@
 ## style-1   style-2   style-3   style-4   style-5
 
 # Current Theme
-dir="$HOME/.config/rofi/powermenu/type-3"
+dir="$HOME/Documents/dots/nix/rofi/powermenu/type-3"
 theme='style-3'
 
 # CMDs
