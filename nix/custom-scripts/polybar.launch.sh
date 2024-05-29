38e466050c59e1f31fba4053d7de3d1691915f61
Crem <maxcaminer1@gmail.com>
2024-05-30 00:14:28 +1000

    catppuccin everywhere and images v2

--- nix/custom-scripts/polybar.launch.sh
+++ nix/custom-scripts/polybar.launch.sh
@@ -4,4 +4,4 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 # Launch: 'top' is the name of my Polybar
-polybar &
+polybar --config=/home/crem/Documents/dots/nix/polybar/config.ini &
